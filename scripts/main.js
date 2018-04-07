156770ddfd091116f7d12121f17e0ee1cf2963da
Adrian <Rodriguez.Adrian609@gmail.com>
2018-04-07 19:12:39 -0400

    Working on deleteSelected() function and its event. LIFO and FIFO working but further testing is neccesary. Local Storage usage and perofmance needs to be assessed during performance and UNIT testing.

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,70 @@
+
 document.getElementById("submit-btn").addEventListener("click", (event) => {
     event.preventDefault();
     let getTextArea = document.getElementById("textArea");
     let inputText = document.getElementById("textArea");
     localStorage.setItem("textArea", inputText.value);
-
     lifo(getTextArea);
     fifo(getTextArea);
-    
+    document.getElementById("deleteBtn").addEventListener("click", deleteSelected);
 });
+document.getElementById("btnReset").addEventListener("click", deleteAll);
+
+function deleteSelected() {
+    var li = document.querySelector("li");
+    li.remove();
+}
+function deleteAll() {
+    var deleteList = document.getElementsByTagName("li")
+    while (deleteList.length > 0) {
+        var li = document.getElementById("listItem");
+        li.remove();
+        console.log(li, deleteList);
+    }
+}
 
 function lifo(getTextArea) {
     let storedText = localStorage.getItem("textArea");
     let list = document.getElementById("list");
     let li = document.createElement("li");
-    li.id="lifo-list";
-
-    li.innerText = storedText;
+    li.id = "listItem";
+    li.className = "list-group-item";
+    li.innerHTML = `${storedText}<button"type="button" id="deleteBtn" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>`;
     list.appendChild(li);
     deleteFirstLast();
 }
-
 function fifo(getTextArea) {
     let storedText = localStorage.getItem("textArea");
     let list = document.getElementById("list-2");
     let li = document.createElement("li");
-    li.id = "fifo-list";
-
-    li.innerText = storedText;
+    li.id = "listItem";
+    li.className = "list-group-item";
+    li.innerHTML = `${storedText}<button type="button" id="deleteBtn" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>`;
     list.appendChild(li);
 }
+function deleteFirstLast() {
+    var first = document.getElementsByTagName("li")[0];
+    var last = document.getElementsByTagName("li")[10];
+    var length = document.getElementsByTagName("li").length;
 
-function deleteFirstLast(){
-    //var z = document.getElementById("fifo-list");
-    var x = document.getElementsByTagName("li")[0];
-    var q = document.getElementsByTagName("li")[10];
-    var y = document.getElementsByTagName("li").length;
-    console.log(x,y);
-    if(y>=10){
-        x.remove();
-        q.remove();
+    if (length >= 10 && first[0]!=0 ) {
+        last.remove();
+        first.remove();
+    }else if(length >= 10){
+        last.remove();
+    }else if(first[0]!=0 && length >= 10){
+        first.remove();
     }else{
-        console.log("y: "+y);
+        console.log("Ran Fine");
     }
-
-    console.log(x,y);
     
 }
+
+
+
+
+
+
 
 
 
