d8f4e04aecb901a185d58fc811f887e118e55467
Adrian <Rodriguez.Adrian609@gmail.com>
2018-04-07 11:41:48 -0400

    Implemented deleting first and last elements in <li> for FIFO and LIFO

--- scripts/main.js
+++ scripts/main.js
@@ -6,23 +6,45 @@ document.getElementById("submit-btn").addEventListener("click", (event) => {
 
     lifo(getTextArea);
     fifo(getTextArea);
-
+    
 });
 
 function lifo(getTextArea) {
     let storedText = localStorage.getItem("textArea");
     let list = document.getElementById("list");
     let li = document.createElement("li");
+    li.id="lifo-list";
+
     li.innerText = storedText;
     list.appendChild(li);
+    deleteFifo();
 }
 
 function fifo(getTextArea) {
     let storedText = localStorage.getItem("textArea");
     let list = document.getElementById("list-2");
     let li = document.createElement("li");
+    li.id = "fifo-list";
+
     li.innerText = storedText;
     list.appendChild(li);
+}
+
+function deleteFifo(){
+    var z = document.getElementById("fifo-list");
+    var x = document.getElementsByTagName("li")[0];
+    var q = document.getElementsByTagName("li")[10];
+    var y = document.getElementsByTagName("li").length;
+    console.log(x,y);
+    if(y>=10){
+        x.remove();
+        q.remove();
+    }else{
+        console.log("y: "+y);
+    }
+
+    console.log(x,y);
+    
 }
 
 
